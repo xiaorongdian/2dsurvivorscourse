59c652fb8a0a6c5ecabb105036756084f370e06b
xiaorongdian <646838688@qq.com>
2025-03-19 21:03:36 +0800

    第一个敌人，敌人与玩家不碰撞，始终朝向玩家移动

---
+++ scenes/basic_enemy/basic_enemy.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+var MAX_SPEED = 75
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var movme_vector = get_vactor_to_player()
+	var direction = movme_vector.normalized()
+	velocity = direction * MAX_SPEED
+	move_and_slide()
+
+func get_vactor_to_player():
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if(player != null):
+		return player.global_position - global_position
+	return Vector2.ZERO
