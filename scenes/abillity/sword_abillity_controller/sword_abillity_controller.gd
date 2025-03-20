9cb80cd7ef31c690a49d327c07499f2a357addea
xiaorongdian <646838688@qq.com>
2025-03-20 22:25:58 +0800

    剑在最近的敌人身上挥舞

--- scenes/abillity/sword_abillity_controller/sword_abillity_controller.gd
+++ scenes/abillity/sword_abillity_controller/sword_abillity_controller.gd
@@ -1,5 +1,6 @@
 extends Node
 
+const MAX_RANGE = 150
 @export var sword_abillity: PackedScene
 
 # Called when the node enters the scene tree for the first time.
@@ -14,13 +15,24 @@ func _process(delta: float) -> void:
 
 
 func on_timer_timeout():
-	print("timeout")
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if(null == player):
 		return
+	var enmies = get_tree().get_nodes_in_group("enmy")
+	enmies.filter(func (enmy : Node2D):
+		return enmy.global_position.distance_squared_to(player.global_position) < pow(MAX_RANGE, 2)
+	)
+	
+	if(enmies.size() == 0):
+		return
+	enmies.sort_custom(func (a : Node2D, b : Node2D):
+		var a_position = a.global_position.distance_squared_to(player.global_position)
+		var b_position = b.global_position.distance_squared_to(player.global_position)
+		return a_position < b_position
+		)
 	
 	var sword_instance = sword_abillity.instantiate() as Node2D
 	player.get_parent().add_child(sword_instance)
-	sword_instance.global_position = player.global_position
+	sword_instance.global_position = enmies[0].global_position
 	
 	
