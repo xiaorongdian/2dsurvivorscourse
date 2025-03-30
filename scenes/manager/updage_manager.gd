0f45b05ce58303d0f67b0b8c9e436a4fb90d4c8c
xiaorongdian <646838688@qq.com>
2025-03-30 21:25:53 +0800

    升级ui暂停鼠标左键点击后游戏继续

--- scenes/manager/updage_manager.gd
+++ scenes/manager/updage_manager.gd
@@ -13,13 +13,17 @@ func _ready() -> void:
 	experience_manager.level_up.connect(on_level_up)
 
 func on_level_up(new_level: int):
+	print("收到升級信號")
+	#随机选一个能力在屏幕显示
 	var chosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
+	print("选一个能力",chosen_upgrade)
 	if chosen_upgrade == null:
 		return
 	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
 	#加到场景树中就行
 	add_child(upgrade_screen_instance)
 	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_selected)
 	#apply_upgrade(chosen_upgrade)
 
 
@@ -32,3 +36,10 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 		}
 	else:
 		current_upgrades[upgrade.id]["quantity"] += 1
+
+
+func on_upgrade_selected(upgrade: AbilityUpgrade):
+	apply_upgrade(upgrade)
+	get_tree().paused = false
+	queue_free()
+	
--- scenes/ui/upgrade_screen.gd
+++ scenes/ui/upgrade_screen.gd
@@ -4,6 +4,8 @@ extends CanvasLayer
 
 @onready var card_container: HBoxContainer = $%CardContainer
 
+signal upgrade_selected(upgrade: AbilityUpgrade)
+
 
 func _ready() -> void:
 	#暂停场景树
@@ -15,4 +17,9 @@ func set_ability_upgrades(upgrades: Array[AbilityUpgrade]):
 		var card_instance = upgrade_scene.instantiate()
 		card_container.add_child(card_instance)
 		card_instance.set_ability_upgrade(upgrade)
+		card_instance.selected.connect(on_upgrade_selected.bind(upgrade))
+	
+func on_upgrade_selected(upgrade : AbilityUpgrade):
+	upgrade_selected.emit(upgrade)
+	
 	
