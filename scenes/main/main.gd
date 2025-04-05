72b1f54bbca74b9829fb06fc253c02da08792606
xiaorongdian <646838688@qq.com>
2025-04-05 20:18:59 +0800

    失败的ui

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -1,18 +1,12 @@
 extends Node
 
-@onready var timer = $Timer
-
 @export var end_screen_scene: PackedScene
 
-
 func _ready() -> void:
-	timer.timeout.connect(on_timer_timeout)
-
-
-func get_time_elapsed():
-	return $Timer.wait_time - $Timer.time_left
-
-
-func on_timer_timeout():
+	$%Player.health_component.died.connect(on_player_died)
+	
+	
+func on_player_died	():
 	var end_screen_instance = end_screen_scene.instantiate()
+	end_screen_instance.set_defeat()
 	add_child(end_screen_instance)
--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -1,10 +1,15 @@
 extends CanvasLayer
-
+#失败了
 
 func _ready() -> void:
 	get_tree().paused = true
 	$%ResartButton.pressed.connect(on_resart_button_pressed)
 	$%QuitButton.pressed.connect(on_quit_button_pressed)
+
+
+func set_defeat():
+	$%TitleLabel.text = "失败"
+	$%DescriptionLabel.text = "差一点就成功啦!"
 
 
 func on_resart_button_pressed():
