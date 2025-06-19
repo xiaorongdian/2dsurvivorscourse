ea32f117e544a77efaef5a43a20b0a92e6a7ba70
xiaorongdian <646838688@qq.com>
2025-06-19 17:49:03 +0800

    敌人死亡动画

---
+++ scenes/component/death_component.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+#健康组件
+@export var health_component: Node
+#纹理
+@export var sprite: Sprite2D
+
+
+func _ready() -> void:
+	$GPUParticles2D.texture = sprite.texture
+	health_component.died.connect(on_died)
+
+
+func on_died():
+	if owner == null || not owner is Node2D:
+		return
+	#var spawn_position = owner.global_position
+	#因为把自己移除后就不能get_tree了，所以先获取到tree
+	var entities = get_tree().get_first_node_in_group("entities_layer")
+	#将自己的父节点重新设置成 entities，两种方式self.reparent(entities)是4.0才有的新方法
+	self.reparent(entities)
+	#get_parent().remove_child(self)
+	#entities.add_child(self)
+	#global_position = spawn_position
+	$AnimationPlayer.play("defalut")
--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -3,12 +3,20 @@ extends CharacterBody2D
 var MAX_SPEED = 40
 
 @onready var health_component: HealthComponent = $HealthComponent
+@onready var visuals = $Visuals
 
 
 func _process(delta: float) -> void:
 	var movme_vector = get_vactor_to_player()
 	velocity = movme_vector * MAX_SPEED
 	move_and_slide()
+	
+	#根据移动方向纹理旋转一下
+	var move_sign = sign(velocity.x)
+	if move_sign != 0:
+		#方式是父节点缩放-1，这样纹理和动画都翻转
+		visuals.scale = Vector2(-move_sign, 1)
+		
 
 func get_vactor_to_player():
 	var player = get_tree().get_first_node_in_group("player") as Node2D
