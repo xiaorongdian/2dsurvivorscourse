26e9f4bd1273db7d1ebbd255ec1f25f2b9db59bf
xiaorongdian <646838688@qq.com>
2025-07-04 05:39:26 +0800

    胜利/失败 ui的动画效果Tween.EASE_OUT，字体轮廓

--- scenes/ui/victory_screen.gd
+++ scenes/ui/victory_screen.gd
@@ -1,7 +1,17 @@
 extends CanvasLayer
 #失败了
 
+@onready var panel_container = $%PanelContainer
+
+
 func _ready() -> void:
+	#将轴心设置到中间
+	panel_container.pivot_offset = panel_container.size / 2
+	var tween = create_tween()
+	tween.tween_property(panel_container, "scale", Vector2.ZERO, 0)
+	tween.tween_property(panel_container, "scale", Vector2.ONE, .3)\
+	.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BACK)
+	
 	get_tree().paused = true
 	$%ResartButton.pressed.connect(on_resart_button_pressed)
 	$%QuitButton.pressed.connect(on_quit_button_pressed)
